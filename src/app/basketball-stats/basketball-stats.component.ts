347f0df7f5f799994886f585b2410144ab986183
Dani Balavac <djani-b@live.com>
2022-06-30 14:37:30 +0200

    msg

--- src/app/basketball-stats/basketball-stats.component.ts
+++ src/app/basketball-stats/basketball-stats.component.ts
@@ -9,7 +9,8 @@ import { ApibasketballService } from '../services/apibasketball.service';
 export class BasketballStatsComponent implements OnInit {
  data! : any;
  meta! : any;
- public point =Math.floor(Math.random()* (8-2));
+ c_page: number = 1;
+ isDisabled: boolean = false;
   constructor(private api : ApibasketballService) { }
 
   ngOnInit(): void {
@@ -17,15 +18,31 @@ export class BasketballStatsComponent implements OnInit {
   }
 
   renderData(){
-    this.api.getStats()
+    this.api.getStats(this.c_page)
     .subscribe(res => {
       this.data = res.data;
       this.meta = res.meta;
       console.log(this.data, this.meta)
     })
   }
-  getRandom(point: number){
-    return this.point * (point);
-    
+  nextPage(){
+    console.log(this.c_page)
+  /*   if(this.c_page === 1) { this.c_page = 2} */
+    this.c_page ++;
+    this.renderData();
+   
   }
+  previousPage(){
+    this.c_page --;
+    if(this.c_page < 1) {
+      this.isDisabled = true;
+      return;
+    }
+    this.renderData();
+  }
+  /* .afterClosed().subsccribe(res =>{
+    this.renderData()
+  }) */
+  
 }
+
--- src/app/services/apibasketball.service.ts
+++ src/app/services/apibasketball.service.ts
@@ -8,14 +8,14 @@ export class ApibasketballService {
 
   constructor(private http: HttpClient) { }
 
-  getStats(){
+  getStats(next: number){
     return this.http.get<any>('https://free-nba.p.rapidapi.com/stats' ,{
       headers: new HttpHeaders()
       .set( 'X-RapidAPI-Host', 'free-nba.p.rapidapi.com')
       .set('X-RapidAPI-Key', '64a8892e4fmshd7b99b6842df0c7p1ea89ejsn323053caf5e9'),
       params: new HttpParams()
-      .set('page', 0)
-      .set('per_page', 20)
+      .set('page', next)
+      .set('per_page', 5)
     })
   }
 }
